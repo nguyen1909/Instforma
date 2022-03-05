c8ce9449b189f8858bd4a92daaeddcc313dc63a5
David NGUYEN <nguyen.david.1909@gmail.com>
2022-03-05 18:30:27 +0100

    Add buttons

--- Instforma/InsformaFramework/Form.swift
+++ Instforma/InsformaFramework/Form.swift
@@ -10,27 +10,24 @@ import UIKit
 
 public class Form {
     
-    //Déclarer un tableau d'input par default
-    //var defaultInputs: [UITextField] = [emailTextField, passwordTextField]
-    var defaultInputs: [UITextField] = []
     var inputs: [UITextField]
     var theme: Theme!
     var isNFC: Bool
     
-    init(theme: Theme, inputs: [UITextField], isNFC: Bool){
+    init(theme: Theme, inputs: [UITextField] ,isNFC: Bool){
         self.inputs = inputs
         self.theme = theme
         self.isNFC = isNFC
     }
     
     init(theme: Theme, isNFC: Bool){
-        self.inputs = self.defaultInputs
+        self.inputs = []
         self.theme = theme
         self.isNFC = isNFC
     }
     
     init(theme: Theme){
-        self.inputs = self.defaultInputs
+        self.inputs = []
         self.theme = theme
         self.isNFC = false
     }
@@ -51,31 +48,62 @@ public class Form {
     }
     
     private func buildSingIn(targetView: UIView){
-        if(self.inputs.count > 0){
-            for input in inputs {
-                //Tout les parametres d'un input
-                input.textColor = self.theme.textColor
-                input.backgroundColor = self.theme.backgroundColor
-                input.frame.size.width = CGFloat(self.theme.inputWidth)
-                input.frame.size.height = CGFloat(self.theme.inputHeight)
-                
-                targetView.addSubview(input)
-            }
+        
+        if(self.inputs.isEmpty){
+            let emailTextField = UITextField()
+            let passwordTextField = UITextField()
+            
+            self.inputs.append(emailTextField)
+            self.inputs.append(passwordTextField)
         }
+        
+        for input in inputs {
+            targetView.addSubview(input)
+        }
+        let buttonSignin = UIButton()
+        targetView.addSubview(buttonSignin)
+    }
+    
+    private func applyThemeToInput(inputs: [UITextField]){
+        
+        for input in inputs {
+            //Tout les parametres d'un input
+            input.textColor = self.theme.textColor
+            input.backgroundColor = self.theme.backgroundColor
+            input.frame.size.width = CGFloat(self.theme.inputWidth)
+            input.frame.size.height = CGFloat(self.theme.inputHeight)
+            
+        }
+        
     }
     
     private func buildSingUp(targetView: UIView){
-        if(self.inputs.count > 0){
-            for input in inputs {
-                //Tout les parametres d'un input
-                input.textColor = self.theme.textColor
-                input.backgroundColor = self.theme.backgroundColor
-                input.frame.size.width = CGFloat(self.theme.inputWidth)
-                input.frame.size.height = CGFloat(self.theme.inputHeight)
-                
-                targetView.addSubview(input)
-            }
+        if(self.inputs.isEmpty){
+            let nameTextField = UITextField()
+            let emailTextField = UITextField()
+            let passwordTextField = UITextField()
+            let gender = UITextField()
+            
+            self.inputs.append(nameTextField)
+            self.inputs.append(emailTextField)
+            self.inputs.append(passwordTextField)
+            self.inputs.append(gender)
         }
+        
+        for input in inputs {
+            targetView.addSubview(input)
+        }
+        
+        let buttonSignup = UIButton()
+        buttonSignup.backgroundColor = self.theme.backgroundColor
+        buttonSignup.frame.size.width = CGFloat(self.theme.inputWidth)
+        buttonSignup.frame.size.height = CGFloat(self.theme.inputHeight)
+        buttonSignup.titleLabel?.textColor = self.theme.textColor
+        //buttonSignup.layer.borderColor = CGColor(self.theme.baseColor)
+        buttonSignup.layer.cornerRadius = CGFloat(self.theme.inputRadius)
+        //buttonSignup.layoutMargins = UIEdgeInsets(self.theme.inputMargin)
+
+        targetView.addSubview(buttonSignup)
     }
     
     func checkView(targetView : UIView) -> Bool{
